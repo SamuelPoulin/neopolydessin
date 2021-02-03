53605a4129af86883e0748e9e46fc09e7ca77272
antoine <antoine.morcel@polymtl.ca>
2021-02-03 00:51:26 -0500

    P-19 fix du linting

--- server/app/socketio.ts
+++ server/app/socketio.ts
@@ -1,24 +1,24 @@
-import 'reflect-metadata';
-import { injectable } from "inversify";
-import * as socketio from 'socket.io';
 import * as http from 'http';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
+import { Server, Socket } from 'socket.io';
 
 @injectable()
 export class SocketIo {
 
-    io: socketio.Server;
+    io: Server;
 
     init(server: http.Server): void {
-        this.io = require('socket.io')(server, {
+        this.io = new Server(server, {
             cors: {
-                origin: "*"
+                origin: '*'
             }
-        })
+        });
         this.bindIoEvents();
     }
 
     bindIoEvents(): void {
-        this.io.on('connection', (socket: socketio.Socket) => {
+        this.io.on('connection', (socket: Socket) => {
             console.log(`Connected with ${socket.id} \n`);
 
             socket.on('disconnect', () => {
