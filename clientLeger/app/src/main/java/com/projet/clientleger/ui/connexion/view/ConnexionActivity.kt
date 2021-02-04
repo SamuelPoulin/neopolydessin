9a963416899d141732aac110a2bb221e9ec92a93
= <guillaume.beausoleil@polymtl.ca>
2021-02-03 21:34:54 -0500

    P-23-24 setup structure de ficher

--- clientLeger/app/src/main/java/com/projet/clientleger/ui/connexion/view/ConnexionActivity.kt
+++ clientLeger/app/src/main/java/com/projet/clientleger/ui/connexion/view/ConnexionActivity.kt
@@ -1,11 +1,12 @@
-package com.projet.clientleger
+package com.projet.clientleger.ui.connexion.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.projet.clientleger.R
 
-class MainActivity : AppCompatActivity() {
+class ConnexionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.connexion_activity)
     }
 }
