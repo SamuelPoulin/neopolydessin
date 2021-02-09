7337a9dc12cc7d77360cce17a01e620ae439efe1
Simon <simon.malouin@polymtl.ca>
2021-02-09 01:11:08 -0500

    P-11 Added node caching

--- pipeline/test_electron.sh
+++ pipeline/test_electron.sh
@@ -5,5 +5,6 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 echo "$USER"
 cd client
+npm ci --cache .npm --prefer-offline
 npm ci
 npm run test
--- pipeline/test_server.sh
+++ pipeline/test_server.sh
@@ -2,5 +2,6 @@
 
 echo "$USER"
 cd server
+npm ci --cache .npm --prefer-offline
 npm ci
 npm run test
