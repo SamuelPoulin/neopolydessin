4c71dd7884f28aca7f92f0d0889859af016c7646
planteetienne <etienne-2.plante@polymtl.ca>
2021-02-14 00:18:54 -0500

    P-43 setup du xml du menu de recherche de partie

---
+++ clientLeger/app/src/main/java/com/projet/clientleger/ui/connexion/view/SearchLobbyActivity.kt
@@ -0,0 +1,13 @@
+package com.projet.clientleger.ui.connexion.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.projet.clientleger.R
+
+class SearchLobbyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_lobby)
+    }
+}
