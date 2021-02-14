49c8c465f89a12bf2db950da4bb66f264d0b90df
planteetienne <etienne-2.plante@polymtl.ca>
2021-02-14 01:30:29 -0500

    P-43 creation d'un adapter pour le recycler view de l'outil de selection de partie

---
+++ clientLeger/app/src/main/java/com/projet/clientleger/data/model/GameInfo.kt
@@ -0,0 +1,8 @@
+package com.projet.clientleger.data.model
+
+@Serializable
+data class GameInfo(override var lobbyName: String, override var lobbyOwner: String, override var gameMode: String, override var gameCapacity: String): IGameInfo {
+    override fun toString(): String {
+        return "Lobby Name: $lobbyName, Lobby Owner: $lobbyOwner, Game Mode: $gameMode, Game Capacity: $gameCapacity"
+    }
+}
---
+++ clientLeger/app/src/main/java/com/projet/clientleger/data/model/IGameInfo.kt
@@ -0,0 +1,8 @@
+package com.projet.clientleger.data.model
+
+interface IGameInfo {
+    var lobbyName: String
+    var lobbyOwner: String
+    var gameMode: String
+    var gameCapacity: String
+}
