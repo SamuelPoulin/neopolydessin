f38be0fe1264f6b54431548b901f0f37bb7ac4ab
MaximeFecteau <maxime.fecteau@polymtl.ca>
2021-02-17 00:03:31 -0500

    P-30-35 fix send message to specific room lint

--- server/app/services/socket-id.service.ts
+++ server/app/services/socket-id.service.ts
@@ -2,22 +2,22 @@ import { injectable } from 'inversify';
 
 @injectable()
 export class SocketIdService {
-    socketIdNameMap: Map<string, string>;
+  socketIdNameMap: Map<string, string>;
 
-    constructor() {
-        this.socketIdNameMap = new Map<string, string>();
-    }
+  constructor() {
+    this.socketIdNameMap = new Map<string, string>();
+  }
 
-    AssociateSocketIdName(socketId: string, name: string): void {
-        this.socketIdNameMap.set(socketId, name);
-    }
+  AssociateSocketIdName(socketId: string, name: string): void {
+    this.socketIdNameMap.set(socketId, name);
+  }
 
-    DisconnectSocketIdName(socketId: string): void {
-        this.socketIdNameMap.delete(socketId);
-    }
+  DisconnectSocketIdName(socketId: string): void {
+    this.socketIdNameMap.delete(socketId);
+  }
 
-    GetNameOfSocketId(socketId: string): string {
-        // tslint:disable-next-line:no-non-null-assertion
-        return this.socketIdNameMap.get(socketId)!;
-    }
+  GetNameOfSocketId(socketId: string): string {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return this.socketIdNameMap.get(socketId)!;
+  }
 }
