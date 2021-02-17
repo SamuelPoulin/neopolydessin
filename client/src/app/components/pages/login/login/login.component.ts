e56df00a5ca7a6d5020458f9a08b6b6277e9e53f
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-02-17 00:05:03 -0500

    P-29-44 added snaccbar

--- client/src/app/components/pages/login/login/login.component.ts
+++ client/src/app/components/pages/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { SocketService } from '@services/socket-service.service';
 import { UserService } from '@services/user.service';
@@ -10,17 +11,26 @@ import { UserService } from '@services/user.service';
 })
 export class LoginComponent {
   inputValue: string = '';
+  currentError: string = '';
 
-  constructor(private socketService: SocketService, private router: Router, private userService: UserService) {}
+  constructor(
+    private socketService: SocketService,
+    private router: Router,
+    private userService: UserService,
+    private snackBar: MatSnackBar
+  ) {}
 
   login() {
     this.socketService.newPlayer(this.inputValue).then((valid) => {
       if (valid) {
         this.userService.username = this.inputValue;
         this.router.navigate(['chat']);
-      }
-      else {
-        console.log('Go back to the ... MEGA GAYZONE');
+      } else {
+        this.snackBar.open("Ce nom d'utilisateur est non disponible.", 'Ok', {
+          duration: 2000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+        });
       }
     });
   }
