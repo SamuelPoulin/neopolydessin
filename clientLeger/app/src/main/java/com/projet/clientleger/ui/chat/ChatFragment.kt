5995228f39576857d28719f650f37c4020190474
= <guillaume.beausoleil@polymtl.ca>
2021-02-19 19:25:54 -0500

    P-31 trim spaces

--- clientLeger/app/src/main/java/com/projet/clientleger/ui/chat/ChatFragment.kt
+++ clientLeger/app/src/main/java/com/projet/clientleger/ui/chat/ChatFragment.kt
@@ -22,12 +22,15 @@ import com.projet.clientleger.data.model.IMessage
 import com.projet.clientleger.data.model.MessageChat
 import kotlinx.android.synthetic.main.fragment_chat.*
 import java.util.regex.Pattern
+
 private const val USERNAME_FORMAT_ERROR: String = "Doit contenir seulement 10 lettres ou chiffres."
 private const val USERNAME_UNICITY_ERROR: String = "Pseudonyme déjà utilisé, choisir un autre !"
-private const val MESSAGE_CONTENT_ERROR: String = "Le message ne doit pas être vide et ne doit pas dépasser 200 caractères"
+private const val MESSAGE_CONTENT_ERROR: String =
+    "Le message ne doit pas être vide et ne doit pas dépasser 200 caractères"
 private const val CHOOSING_USERNAME_TITLE: String = "Choisir son pseudonyme"
-private const val CONNECT_BUTTON_TITLE:String = "Connect"
-private const val USERNAME_INPUT_HINT:String = "Nom d'utilisateur"
+private const val CONNECT_BUTTON_TITLE: String = "Connect"
+private const val USERNAME_INPUT_HINT: String = "Nom d'utilisateur"
+
 class ChatFragment() : Fragment() {
     private lateinit var connexionDialog: AlertDialog
     private var messages: ArrayList<IMessage> = ArrayList()
@@ -86,7 +89,7 @@ class ChatFragment() : Fragment() {
     }
 
     private fun chooseUsername(username: String) {
-        if(!isUsernameValidFormat(username)) {
+        if (!isUsernameValidFormat(username)) {
             showErrorToast(USERNAME_FORMAT_ERROR)
             return
         }
@@ -132,43 +135,55 @@ class ChatFragment() : Fragment() {
 
     private fun sendButton() {
         val text: String = (chatBox.text).toString()
-        val adjustedText: String = text.replace("\\s+".toRegex(), " ")
-        if(isMessageValidFormat(adjustedText))
-        {
+        val adjustedText: String = formatMessageContent(text)
+        if (isMessageValidFormat(adjustedText)) {
             addMessage(MessageChat(username, adjustedText, System.currentTimeMillis()))
-            socketService?.sendMessage(MessageChat(username, adjustedText, System.currentTimeMillis()))
-        }
-        else{
+            socketService?.sendMessage(
+                MessageChat(
+                    username,
+                    adjustedText,
+                    System.currentTimeMillis()
+                )
+            )
+        } else {
             showErrorToast(MESSAGE_CONTENT_ERROR)
         }
         chatBox.text?.clear()
     }
 
     private fun addMessage(message: IMessage) {
-            messages.add(message)
-            activity?.runOnUiThread {
-                //rvMessages.adapter?.notifyItemInserted(messages.size - 1)
-                rvMessages.adapter?.notifyDataSetChanged()
-                rvMessages.scrollToPosition(messages.size - 1)
-            }
+        message.content = formatMessageContent(message.content)
+        messages.add(message)
+        activity?.runOnUiThread {
+            //rvMessages.adapter?.notifyItemInserted(messages.size - 1)
+            rvMessages.adapter?.notifyDataSetChanged()
+            rvMessages.scrollToPosition(messages.size - 1)
+        }
     }
 
-    private fun isUsernameValidFormat(username: String): Boolean{
+    private fun isUsernameValidFormat(username: String): Boolean {
         return Pattern.matches("^[A-Za-z0-9. ]+(?:[_&%$*#@!-][A-Za-z0-9. ]+)*$", username)
     }
-    private fun isMessageValidFormat(message: String): Boolean{
+
+    private fun isMessageValidFormat(message: String): Boolean {
         return Pattern.matches(".*\\S.*", message) && message.length <= 200 && message.isNotEmpty()
     }
 
-    private fun showErrorToast(errorMessage: String){
+    private fun formatMessageContent(messageContent: String): String {
+        var adjustedText: String = messageContent.replace("\\s+".toRegex(), " ")
+        adjustedText = adjustedText.trimStart()
+        return adjustedText
+    }
+
+    private fun showErrorToast(errorMessage: String) {
         activity?.runOnUiThread {
             Toast.makeText(
-                    activity,
-                    Html.fromHtml(
-                            "<font color='#e61515'><b>${errorMessage}</b></font>",
-                            0
-                    ),
-                    Toast.LENGTH_SHORT
+                activity,
+                Html.fromHtml(
+                    "<font color='#e61515'><b>${errorMessage}</b></font>",
+                    0
+                ),
+                Toast.LENGTH_SHORT
             ).show()
         }
     }
@@ -182,7 +197,7 @@ class ChatFragment() : Fragment() {
     }
 
     override fun onDestroy() {
-        if(this::connexionDialog.isInitialized)
+        if (this::connexionDialog.isInitialized)
             connexionDialog.dismiss()
         super.onDestroy()
     }
