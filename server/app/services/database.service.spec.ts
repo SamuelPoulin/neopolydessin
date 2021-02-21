fd896df6b31a49bb8816b8f363407f23530a2045
antoine <antoine.morcel@polymtl.ca>
2021-02-20 19:08:08 -0500

    P-68 debut des fix de serveur et ajout des tests pour mon middleware custom

--- server/app/services/database.service.spec.ts
+++ server/app/services/database.service.spec.ts
@@ -1,25 +1,20 @@
-/* tslint:disable:no-any no-magic-numbers */
-/* tslint:disable:no-any no-string-literal */
 import { expect } from 'chai';
 import * as httpStatus from 'http-status-codes';
-
-import drawingModel from '../../models/account';
 import { DatabaseService } from './database.service';
-
 import { testingContainer } from '../../test/test-utils';
 import Types from '../types';
+import { Register } from '../../../common/communication/register';
+// import accountModel from '../../models/account';
 
 describe('Database Service', () => {
   let databaseService: DatabaseService;
-  const testingDrawing = new drawingModel({
-    name: 'test',
-    tags: ['tag1', 'tag2', 'tag3'],
-    data: 'random data',
-    color: '',
-    width: 0,
-    height: 0,
-    previewURL: 'www',
-  });
+
+  // const testAccount = new accountModel({
+  //   name: 'name',
+  //   username: 'username',
+  //   email: 'email@email.email',
+  //   password: 'monkey123',
+  // });
 
   beforeEach(async () => {
     await testingContainer().then((instance) => {
@@ -34,118 +29,19 @@ describe('Database Service', () => {
     done();
   });
 
-  it('should add a drawing correctly', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((result) => {
+  it('should create an account correctly', (done: Mocha.Done) => {
+    const accountInfo: Register = {
+      name: 'name',
+      username: 'username',
+      email: 'email@email.email',
+      password: 'monkey123',
+      passwordConfirm: 'monkey123'
+    }
+    databaseService.createAccount(accountInfo).then((result) => {
       expect(result.statusCode).to.equal(httpStatus.OK);
       expect(result.documents).to.not.equal(null);
-      expect(result.documents.name).to.equal('test');
+      expect(result.documents).to.equal('Account successfully created');
       done();
-    });
-  });
-
-  it('should get all drawings', (done: Mocha.Done) => {
-    databaseService.getAllDrawings().then((result) => {
-      expect(result.statusCode).to.equal(httpStatus.OK);
-      done();
-    });
-  });
-
-  it('should get one drawing after adding it', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then(() => {
-      databaseService.getAllDrawings().then((result) => {
-        expect(result.statusCode).to.equal(httpStatus.OK);
-        expect(result.documents.length).to.equal(1);
-        done();
-      });
-    });
-  });
-
-  it('should report error on incorrect ID format', (done: Mocha.Done) => {
-    databaseService.getDrawingById('randomid').then((result) => {
-      expect(result.statusCode).to.equal(httpStatus.INTERNAL_SERVER_ERROR);
-      done();
-    });
-  });
-
-  it('should report not found on correct ID format', (done: Mocha.Done) => {
-    databaseService.getDrawingById('5e78f886b368e63343f98f8b').then((result) => {
-      expect(result.statusCode).to.equal(httpStatus.NOT_FOUND);
-      done();
-    });
-  });
-
-  it('should find a drawing that was added earlier', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.getDrawingById(addedDoc.documents._id).then((foundDoc) => {
-        expect(addedDoc.documents.name).to.equal(foundDoc.documents.name);
-        done();
-      });
-    });
-  });
-
-  it('should find and delete a drawing that was added earlier', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.deleteDrawing(addedDoc.documents._id).then((deletedDoc) => {
-        expect(addedDoc.documents.name).to.equal(deletedDoc.documents.name);
-        databaseService.getDrawingById(deletedDoc.documents._id).then((foundDoc) => {
-          expect(foundDoc.documents).to.equal(null);
-          done();
-        });
-      });
-    });
-  });
-
-  it('should return the old document on update', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.updateDrawing(addedDoc.documents._id, ({name: 'new name'} as Drawing)).then((oldDoc) => {
-        expect(oldDoc.documents.name).to.equal(addedDoc.documents.name);
-        done();
-      });
-    });
-  });
-
-  it('should return a document when searching by name', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.searchDrawings('test', '').then((foundDoc) => {
-        expect(foundDoc.documents[0].name).to.equal(addedDoc.documents.name);
-        done();
-      });
-    });
-  });
-
-  it('should return a document when searching by one tag', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.searchDrawings('', 'tag1').then((foundDoc) => {
-        expect(foundDoc.documents[0].name).to.equal(addedDoc.documents.name);
-        done();
-      });
-    });
-  });
-
-  it('should return a document when searching by one incomplete tag', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.searchDrawings('', 'tag').then((foundDoc) => {
-        expect(foundDoc.documents[0].name).to.equal(addedDoc.documents.name);
-        done();
-      });
-    });
-  });
-
-  it('should return a document when searching by incomplete tag array', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.searchDrawings('', ['tag', 'tag']).then((foundDoc) => {
-        expect(foundDoc.documents[0].name).to.equal(addedDoc.documents.name);
-        done();
-      });
-    });
-  });
-
-  it('should return a document when searching by tag array', (done: Mocha.Done) => {
-    databaseService.addDrawing(testingDrawing).then((addedDoc) => {
-      databaseService.searchDrawings('', ['tag1', 'tag2']).then((foundDoc) => {
-        expect(foundDoc.documents[0].name).to.equal(addedDoc.documents.name);
-        done();
-      });
     });
   });
 
