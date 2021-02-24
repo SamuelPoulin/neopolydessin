f4610a759f40eb31c147e73ba4c46a629d9d6538
Simon <simon.malouin@polymtl.ca>
2021-02-24 17:47:54 -0500

    P-57 added register component

--- client/src/app/components/pages/login/login.module.ts
+++ client/src/app/components/pages/login/login.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
 })
 export class LoginModule {}
