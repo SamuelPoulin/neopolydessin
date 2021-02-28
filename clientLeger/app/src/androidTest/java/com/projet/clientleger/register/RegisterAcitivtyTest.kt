b5acb7e59424f1fdbaf6bac682d3114c842bbe56
= <guillaume.beausoleil@polymtl.ca>
2021-02-28 16:53:10 -0500

    P-46-80 fix imports

---
+++ clientLeger/app/src/androidTest/java/com/projet/clientleger/register/RegisterAcitivtyTest.kt
@@ -0,0 +1,16 @@
+package com.projet.clientleger.register
+import androidx.test.core.app
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class RegisterAcitivtyTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Test
+    fun idk(){
+    }
+}
