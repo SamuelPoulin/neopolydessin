4367a4702b9e4cc8a38e61648355e947c391ead1
= <guillaume.beausoleil@polymtl.ca>
2021-03-03 17:26:55 -0500

    P-46-61 UI avec changement fragment done

--- clientLeger/app/src/main/java/com/projet/clientleger/ui/accountmanagement/view/AccountManagementActivity.kt
+++ clientLeger/app/src/main/java/com/projet/clientleger/ui/accountmanagement/view/AccountManagementActivity.kt
@@ -10,6 +10,7 @@ import com.projet.clientleger.R
 import com.projet.clientleger.databinding.ActivityAccountManagementBinding
 import com.projet.clientleger.ui.accountmanagement.dashboard.view.DashboardFragment
 import com.projet.clientleger.ui.accountmanagement.profile.ProfileFragment
+import com.projet.clientleger.ui.accountmanagement.settings.SettingsFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -24,6 +25,9 @@ class AccountManagementActivity : AppCompatActivity() {
     @Inject
     lateinit var profileFragment: ProfileFragment
 
+    @Inject
+    lateinit var settingsFragment: SettingsFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,23 +35,24 @@ class AccountManagementActivity : AppCompatActivity() {
         binding = ActivityAccountManagementBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.lifecycleOwner = this
+
         fragmentManager.beginTransaction().add(R.id.fragmentContainer, dashboardFragment, "fragment").commit()
         binding.toggleGroup.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
-            if (!isChecked) {
+            if (isChecked) {
                 when (checkedId) {
                     R.id.dashboardBtn -> changeFragment(dashboardFragment)
                     R.id.userProfileBtn -> changeFragment(profileFragment)
-                    R.id.accountSettings -> changeFragment(null)
+                    R.id.accountSettings -> changeFragment(settingsFragment)
                 }
             }
         }
     }
 
-    fun changeFragment(fragment: Fragment?) {
+    fun changeFragment(fragment: Fragment) {
         val fragmentFound = fragmentManager.findFragmentByTag("fragment")
-        if(fragment != null) {
+        if(fragmentFound != fragment) {
             fragmentManager.beginTransaction()
-                .replace(fragmentFound!!.id, fragment, "fragment")
+                .replace(R.id.fragmentContainer, fragment, "fragment")
                 .commit()
         }
     }
