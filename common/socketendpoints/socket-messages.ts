d9ebb6986c9fe2a3cfd540f638ed6a9d27dd9e2b
MaximeFecteau <maxime.fecteau@polymtl.ca>
2021-03-04 18:38:42 -0500

    P-71 Ajout endpoint pour socketMessages enum

--- common/socketendpoints/socket-messages.ts
+++ common/socketendpoints/socket-messages.ts
@@ -4,5 +4,10 @@ export enum SocketMessages {
     RECEIVE_MESSAGE = 'ReceiveMsg',
     RECEIVE_PRIVATE_MESSAGE = 'ReceivePrivateMsg',
     PLAYER_CONNECTION = 'PlayerConnected',
-    PLAYER_DISCONNECTION = 'PlayerDisconnected'
+    PLAYER_DISCONNECTION = 'PlayerDisconnected',
+    START_GAME_SERVER = 'StartGameFromLobbyToServer',
+    START_GAME_CLIENT = 'StartGameFromServerToClient',
+    CREATE_LOBBY = 'CreateLobby',
+    GET_ALL_LOBBIES = 'GetListLobby',
+    PLAYER_GUESS = 'PlayerGuessWord'
 }
