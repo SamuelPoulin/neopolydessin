c03b25749de7d1d32512c022f09be0cb314122ea
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-03-07 15:57:55 -0500

    P-96 reworked home page

--- client/src/app/components/pages/home/home.module.ts
+++ client/src/app/components/pages/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { CreateDrawingModalComponent } from './create-drawing-modal/create-drawing-modal.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
   declarations: [CreateDrawingModalComponent, HomeComponent],
 })
 export class HomeModule {}
