02829609bd34f827cbdb1e22482be2834a3ab49b
Simon <simon.malouin@polymtl.ca>
2021-03-10 17:19:24 -0500

    P-57 minor test fixes

--- client/src/app/components/pages/login/register/register.component.spec.ts
+++ client/src/app/components/pages/login/register/register.component.spec.ts
@@ -1,4 +1,10 @@
+import { Overlay } from '@angular/cdk/overlay';
+import { HttpXhrBackend } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { APIService } from '@services/api.service';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,7 +14,9 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      providers: [ { provide: HttpXhrBackend, useClass: HttpTestingController }, APIService, MatSnackBar, Overlay ],
+      declarations: [ RegisterComponent ],
+      imports: [ HttpClientTestingModule, RouterTestingModule ]
     })
     .compileComponents();
   });
