48c2fc9ae732340a0e76d892f7955ff0ab636c52
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-03-16 13:56:46 -0400

    P-101 added files

---
+++ client/src/app/components/pages/lobby/lobby.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@components/shared/shared.module';
+import { LobbyComponent } from './lobby/lobby.component';
+
+@NgModule({
+  imports: [SharedModule],
+  declarations: [LobbyComponent],
+})
+export class AccountModule {}
