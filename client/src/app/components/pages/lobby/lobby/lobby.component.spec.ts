37528c480b57929f43cca89be3d82d77ac73c40e
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-03-17 04:30:41 -0400

    P-101 fixed tests

--- client/src/app/components/pages/lobby/lobby/lobby.component.spec.ts
+++ client/src/app/components/pages/lobby/lobby/lobby.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { SharedModule } from '../../../shared/shared.module';
 import { LobbyComponent } from './lobby.component';
+import { StatusBarModule } from '../../../shared/status-bar/status-bar.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LobbyComponent', () => {
   let component: LobbyComponent;
@@ -8,6 +10,7 @@ describe('LobbyComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SharedModule, StatusBarModule, RouterTestingModule],
       declarations: [LobbyComponent],
     }).compileComponents();
   });
--- client/src/app/components/pages/server-browser/server-browser/server-browser.component.spec.ts
+++ client/src/app/components/pages/server-browser/server-browser/server-browser.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SharedModule } from '../../../shared/shared.module';
+import { StatusBarModule } from '../../../shared/status-bar/status-bar.module';
 import { ServerBrowserComponent } from './server-browser.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BrowserComponent', () => {
   let component: ServerBrowserComponent;
@@ -7,6 +10,7 @@ describe('BrowserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SharedModule, StatusBarModule, RouterTestingModule],
       declarations: [ServerBrowserComponent],
     }).compileComponents();
   });
