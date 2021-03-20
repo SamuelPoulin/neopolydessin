bc9b17299558e2bdd3dc3555ab481b8ecb147489
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-03-20 18:29:48 -0400

    P-101 added table to server browser

--- client/src/app/components/pages/server-browser/server-browser/server-browser.component.ts
+++ client/src/app/components/pages/server-browser/server-browser/server-browser.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SocketService } from '@services/socket-service.service';
-import { Subscription } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+// import { Router } from '@angular/router';
+// import { SocketService } from '@services/socket-service.service';
+import { Observable } from 'rxjs';
 import { Difficulty, GameType, LobbyInfo } from '../../../../../../../common/communication/lobby';
 
 @Component({
@@ -10,20 +11,56 @@ import { Difficulty, GameType, LobbyInfo } from '../../../../../../../common/com
   styleUrls: ['./server-browser.component.scss'],
 })
 export class ServerBrowserComponent implements OnInit {
-  displayedColumns: string[] = ['lobbyId'];
-  lobbiesSubscription: Subscription;
-  lobbies: LobbyInfo[] = [];
+  displayedColumns: string[] = ['lobbyId', 'playerInfo', 'gameType', 'joinButton'];
+  dataSource: MatTableDataSource<LobbyInfo>;
+  lobbyCount: number;
 
-  constructor(private socketService: SocketService, private router: Router) {}
+  constructor() // private socketService: SocketService,
+  // private router: Router
+  {
+    this.dataSource = new MatTableDataSource<LobbyInfo>();
+  }
 
   ngOnInit() {
-    this.socketService.getLobbyList(GameType.CLASSIC, Difficulty.EASY).subscribe((lobbies) => {
-      this.lobbies.push(...lobbies);
+    this.getLobbyList(GameType.CLASSIC, Difficulty.EASY).subscribe((lobbies) => {
+      this.dataSource.data = lobbies;
+      this.lobbyCount = lobbies.length;
+    });
+  }
+
+  getLobbyList(gameType: GameType, difficulty: Difficulty): Observable<LobbyInfo[]> {
+    return new Observable<LobbyInfo[]>((msgObs) => {
+      setInterval(() => {
+        this.dummyLobbies().then((lobbies) => {
+          msgObs.next(lobbies);
+        });
+      }, 2000);
+    });
+  }
+
+  async dummyLobbies(): Promise<LobbyInfo[]> {
+    return new Promise<LobbyInfo[]>((resolve, reject) => {
+      const array = [];
+
+      for (let i = 0; i < Math.random() * 100; i++) {
+        array.push({
+          playerInfo: [{ teamNumber: 1, playerName: 'Hello', accountId: '123' }],
+          lobbyId: Math.floor(Math.random() * 1000000000000000000000).toString(),
+          gameType: GameType.CLASSIC,
+        });
+      }
+
+      resolve(array);
     });
   }
 
   joinLobby(lobbyId: string): void {
-    this.socketService.joinLobby(lobbyId);
-    this.router.navigate(['/edit']);
+    console.log(lobbyId);
+    // this.socketService.joinLobby(lobbyId);
+    // this.router.navigate(['/edit']);
+  }
+
+  get showEmptyMessage(): boolean {
+    return !this.lobbyCount || this.lobbyCount <= 0;
   }
 }
