9acdae4d4fdd047ddb40de3b59106122f5a00cc9
antoine <antoine.morcel@polymtl.ca>
2021-03-22 20:37:23 -0400

    P-110 ajout de la lib potrace et prototypage

---
+++ server/types/potrace/index.d.ts
@@ -0,0 +1,28 @@
+declare module 'potrace' {
+  export function trace(
+    file: string | Buffer,
+    options: PosterizeOptions,
+    cb: (err: Error, res: string) => void
+  ): void;
+
+  export function posterize(
+    file: string | Buffer,
+    otions: PosterizeOptions,
+    cb: (err: Error, res: string) => void
+  ): void;
+
+  export interface PosterizeOptions {
+    background?: string;
+    color?: string;
+    threshold?: number;
+    steps?: number;
+    fillStrategy?: FillStrategy;
+  }
+
+  export const enum FillStrategy {
+    FILL_SPREAD = 'spread',
+    FILL_DOMINANT = 'dominant',
+    FILL_MEDIAN = 'median',
+    FILL_MEAN = 'mean',
+  }
+}
