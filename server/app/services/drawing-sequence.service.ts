4eceb6896a3f5b55c6f9fbd01b31bcee49c74add
antoine <antoine.morcel@polymtl.ca>
2021-04-01 17:54:48 -0400

    P-122 algo de conventionnel, random et center first manque pano

--- server/app/services/drawing-sequence.service.ts
+++ server/app/services/drawing-sequence.service.ts
@@ -1,9 +1,8 @@
 import { injectable } from 'inversify';
 import { DEFAULT_BRUSH_INFO, Path } from '../../models/commands/path';
 import { PictureWord } from '../../models/schemas/picture-word-pair';
-import { DrawingSequence, Segment } from '../../../common/communication/drawing-sequence';
+import { Coord, DrawingSequence, Segment } from '../../../common/communication/drawing-sequence';
 import { DrawMode } from '../../../common/communication/draw-mode';
-
 
 @injectable()
 export class DrawingSequenceService {
@@ -27,16 +26,20 @@ export class DrawingSequenceService {
 
     switch (drawing.drawMode) {
       case DrawMode.CONVENTIONAL:
-        stack = paths.map((path) => {
-          return {
-            brushInfo: path.brushInfo ? path.brushInfo : DEFAULT_BRUSH_INFO,
-            path: path.path
-          };
-        });
+        stack = paths.map((path) => this.toSegment(path));
         break;
       case DrawMode.CENTER_FIRST:
+        const center = this.findCenterPointOfDrawing(paths);
+        stack = paths
+          .sort((a, b) => this.distanceBetween(a.path[0], center) - this.distanceBetween(b.path[0], center))
+          .map((path) => this.toSegment(path));
         break;
       case DrawMode.RANDOM:
+        while (paths.length > 0) {
+          const randomIndex = Math.floor(Math.random() * paths.length);
+          const randomPath = paths.splice(randomIndex, 1)[0];
+          stack.push(this.toSegment(randomPath));
+        }
         break;
       case DrawMode.PAN_B_TO_T:
         break;
@@ -49,6 +52,28 @@ export class DrawingSequenceService {
 
     }
     return { stack };
+  }
+
+  private distanceBetween(coord1: Coord, coord2: Coord): number {
+    return ((coord2.x - coord1.x) * (coord2.x - coord1.x)) + ((coord2.y - coord1.y) * (coord2.y - coord1.y));
+  }
+
+  private findCenterPointOfDrawing(paths: Path[]): Coord {
+    let allCoords: Coord[] = [];
+    paths.forEach((path) => {
+      allCoords = allCoords.concat(allCoords, path.path);
+    });
+    // https://stackoverflow.com/questions/4020796/finding-the-max-value-of-an-attribute-in-an-array-of-objects
+    const maxY = allCoords.reduce((a, b) => a.y > b.y ? a : b).y;
+    const maxX = allCoords.reduce((a, b) => a.x > b.x ? a : b).x;
+    return { x: maxX / 2, y: maxY / 2 };
+  }
+
+  private toSegment(path: Path): Segment {
+    return {
+      brushInfo: path.brushInfo ? path.brushInfo : DEFAULT_BRUSH_INFO,
+      path: path.path
+    };
   }
 
 }
