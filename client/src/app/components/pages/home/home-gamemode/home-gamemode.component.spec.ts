df6ef785dcb9b266bf0b173ced83130a49042d34
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-04-07 00:50:28 -0400

    P-125 fixed tests

--- client/src/app/components/pages/home/home-gamemode/home-gamemode.component.spec.ts
+++ client/src/app/components/pages/home/home-gamemode/home-gamemode.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '@components/shared/shared.module';
+import { GameService } from '@services/game.service';
+import { MockGameService } from '@services/game.service.spec';
+import { SocketService } from '@services/socket-service.service';
+import { MockSocketService } from '@services/socket-service.service.spec';
+import { UserService } from '@services/user.service';
+import { MockUserService } from '@services/user.service.spec';
 
 import { HomeGamemodeComponent } from './home-gamemode.component';
 
@@ -8,7 +17,14 @@ describe('HomeGamemodeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SharedModule, RouterTestingModule],
       declarations: [HomeGamemodeComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: SocketService, useValue: MockSocketService },
+        { provide: UserService, useValue: MockUserService },
+        { provide: GameService, useValue: MockGameService },
+      ],
     }).compileComponents();
   });
 
