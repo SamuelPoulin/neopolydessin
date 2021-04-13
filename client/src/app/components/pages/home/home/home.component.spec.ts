16f38819a024a4b741fc0da833092eae04835457
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-04-13 00:24:11 -0400

    P-155 fixed tests

--- client/src/app/components/pages/home/home/home.component.spec.ts
+++ client/src/app/components/pages/home/home/home.component.spec.ts
@@ -2,13 +2,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ChatModule } from '@components/pages/chat/chat.module';
 import { StatusBarModule } from '@components/shared/status-bar/status-bar.module';
+import { ChatService } from '@services/chat.service';
+import { MockChatService } from '@services/chat.service.spec';
+import { MockElectronService } from '@services/electron.service.spec';
 import { GameService } from '@services/game.service';
 import { MockGameService } from '@services/game.service.spec';
 import { SocketService } from '@services/socket-service.service';
 import { MockSocketService } from '@services/socket-service.service.spec';
 import { UserService } from '@services/user.service';
 import { MockUserService } from '@services/user.service.spec';
+import { ElectronService } from 'ngx-electron';
 import { ModalDialogService } from 'src/app/services/modal/modal-dialog.service';
 import { SharedModule } from '../../../shared/shared.module';
 
@@ -23,12 +28,14 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SharedModule, RouterTestingModule, StatusBarModule],
+      imports: [SharedModule, RouterTestingModule, StatusBarModule, ChatModule],
       declarations: [HomeComponent],
       providers: [
         { provide: SocketService, useValue: MockSocketService },
         { provide: UserService, useValue: MockUserService },
         { provide: GameService, useValue: MockGameService },
+        { provide: ChatService, useValue: MockChatService },
+        { provide: ElectronService, useValue: MockElectronService },
         {
           provide: ModalDialogService,
           useValue: modalDialogServiceSpy,
@@ -55,5 +62,4 @@ describe('HomeComponent', () => {
 
     expect(navigateSpy).toHaveBeenCalledWith(['test']);
   });
-
 });
