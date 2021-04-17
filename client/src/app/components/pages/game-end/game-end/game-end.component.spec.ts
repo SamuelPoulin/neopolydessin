13f013e8baa4827595663092b9f7b12fd70536da
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-04-17 02:58:26 -0400

    P-178 fixed tests

--- client/src/app/components/pages/game-end/game-end/game-end.component.spec.ts
+++ client/src/app/components/pages/game-end/game-end/game-end.component.spec.ts
@@ -1,4 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ChatModule } from '@components/pages/chat/chat.module';
+import { SharedModule } from '@components/shared/shared.module';
+import { StatusBarModule } from '@components/shared/status-bar/status-bar.module';
+import { ChatService } from '@services/chat.service';
+import { MockChatService } from '@services/chat.service.spec';
+import { GameService } from '@services/game.service';
+import { MockGameService } from '@services/game.service.spec';
+import { UserService } from '@services/user.service';
+import { MockUserService } from '@services/user.service.spec';
 
 import { GameEndComponent } from './game-end.component';
 
@@ -8,7 +18,13 @@ describe('GameEndComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SharedModule, RouterTestingModule, StatusBarModule, ChatModule],
       declarations: [GameEndComponent],
+      providers: [
+        { provide: GameService, useValue: MockGameService },
+        { provide: ChatService, useValue: MockChatService },
+        { provide: UserService, useValue: MockUserService },
+      ],
     }).compileComponents();
   });
 
