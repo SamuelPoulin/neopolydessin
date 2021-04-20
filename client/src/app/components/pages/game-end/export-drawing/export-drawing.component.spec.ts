cdd17a10b765a6a1d3787ef297b6971cbd4249e0
Simon <simon.malouin@polymtl.ca>
2021-04-19 22:52:01 -0400

    P-145 fixed tests

--- client/src/app/components/pages/game-end/export-drawing/export-drawing.component.spec.ts
+++ client/src/app/components/pages/game-end/export-drawing/export-drawing.component.spec.ts
@@ -1,20 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
+import { EditorService } from '@services/editor.service';
+import { MockEditorService } from '@services/editor.service.spec';
+import { SocketService } from '@services/socket-service.service';
+import { MockSocketService } from '@services/socket-service.service.spec';
+import { of } from 'rxjs';
 
 import { ExportDrawingComponent } from './export-drawing.component';
 
 describe('ExportDrawingComponent', () => {
   let component: ExportDrawingComponent;
   let fixture: ComponentFixture<ExportDrawingComponent>;
+  const MockMatDialogRef = jasmine.createSpyObj('MatDialogRef', {
+    afterOpened: of(),
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ExportDrawingComponent],
+      providers: [
+        { provide: SocketService, useValue: MockSocketService },
+        { provide: EditorService, useValue: MockEditorService },
+        { provide: MatDialogRef, useValue: MockMatDialogRef },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExportDrawingComponent);
     component = fixture.componentInstance;
+    component.editorService.recordedDrawings = [];
     fixture.detectChanges();
   });
 
