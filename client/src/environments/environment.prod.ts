1c0aa0bbc449933c99dfd0d77446d2286184c2f4
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-06-02 22:29:55 -0400

    Switched prod url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,8 +2,6 @@ import { IEnvironment } from 'src/environments/i-environment';
 
 export const environment: IEnvironment = {
   production: true,
-  // apiBaseUrl: 'http://p3-204.duckdns.org/api',
-  // socketUrl: 'http://p3-204.duckdns.org/',
-  apiBaseUrl: 'http://localhost:3205/api',
-  socketUrl: 'http://localhost:3205/',
+  apiBaseUrl: 'https://neopolydessin.samuelpoulin.ca/api',
+  socketUrl: 'https://neopolydessin.samuelpoulin.ca/',
 };
