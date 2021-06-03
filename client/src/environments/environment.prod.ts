4d865c194a2584076788b7249cfec3fddbb789cb
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-06-02 22:27:51 -0400

    Removed .vs

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ import { IEnvironment } from 'src/environments/i-environment';
 
 export const environment: IEnvironment = {
   production: true,
-  apiBaseUrl: 'http://p3-204.duckdns.org/api',
-  socketUrl: 'http://p3-204.duckdns.org/',
+  // apiBaseUrl: 'http://p3-204.duckdns.org/api',
+  // socketUrl: 'http://p3-204.duckdns.org/',
+  apiBaseUrl: 'http://localhost:3205/api',
+  socketUrl: 'http://localhost:3205/',
 };
