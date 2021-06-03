0247d37994c6f272d2d383d79c8e6866a25111a7
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-06-02 22:09:09 -0400

    Removed emailapi

--- server/app/www.ts
+++ server/app/www.ts
@@ -1,19 +1,23 @@
-import fs from 'fs';
-import { Container } from 'inversify';
-import 'reflect-metadata';
-import { containerBootstrapper } from './inversify.config';
-import { Server } from './server';
-import Types from './types';
+import fs from "fs";
+import { Container } from "inversify";
+import "reflect-metadata";
+import { containerBootstrapper } from "./inversify.config";
+import { Server } from "./server";
+import Types from "./types";
 
 void (async () => {
   try {
-    process.env.API_KEY = fs.readFileSync('emailAPI.env').toString().split('@')[0];
-    process.env.MONGODB_KEY = fs.readFileSync('mongo.env').toString();
-    const jwtkeys = fs.readFileSync('jwtsecret.env').toString().split('\n');
+    process.env.MONGODB_KEY = fs.readFileSync("mongo.env").toString();
+    const jwtkeys = fs
+      .readFileSync("jwtsecret.env")
+      .toString()
+      .split("\n");
     process.env.JWT_KEY = jwtkeys[0];
     process.env.JWT_REFRESH_KEY = jwtkeys[1];
   } catch (err) {
-    console.log('Could not load an API env file. Make sure env files are under server/ Exiting...');
+    console.log(
+      "Could not load an API env file. Make sure env files are under server/ Exiting..."
+    );
     process.exit(0);
   }
 
