441d93bc7041c173d0e780d3afb74ce096710436
Samuel Poulin <samuel.poulin@polymtl.ca>
2021-06-03 01:59:16 -0400

    Reverted socketio path change

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from 'src/environments/i-environment';
 export const environment: IEnvironment = {
   production: true,
   apiBaseUrl: 'https://neopolydessin.samuelpoulin.ca/api',
-  socketUrl: 'https://neopolydessin.samuelpoulin.ca/socket.io',
+  socketUrl: 'https://neopolydessin.samuelpoulin.ca/',
 };
